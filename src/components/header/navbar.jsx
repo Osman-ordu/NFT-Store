b821cbb3b448372e7e3f8371f8da121514014f83
Osman Ordu <orduosmann@gmail.com>
2022-10-23 12:34:01 +0300

    added scroll reset on link transitions

--- src/components/header/navbar.jsx
+++ src/components/header/navbar.jsx
@@ -9,6 +9,9 @@ import { FaPaw } from 'react-icons/fa';
 
 const Header = () => {
     const [collapsed, setCollapsed] = useState(true);
+    const scrollToTop = () => {
+        document.getElementById('scroller').scroll(0,0)
+    }
 
     const toggleNavbar = () => setCollapsed(!collapsed);
     return (
@@ -17,26 +20,26 @@ const Header = () => {
                 <NavbarBrand  className="me-auto">
                     <img className="logo-item" src={logo} alt='logo' />
                 </NavbarBrand>
-                <button onClick={toggleNavbar} type='button' className='btn btn-collapse shadow-none'><BiCode size={30} /></button>
+                <button onClick={toggleNavbar} type='button'  className='btn btn-collapse shadow-none'><BiCode size={30} /></button>
                 <Collapse isOpen={!collapsed} navbar>
                     <Nav navbar>
                         <NavItem>
-                            <NavLink to='/homepage' className='nav-link fw-bold border border-dark border-1 rounded p-2 text-center'>
+                            <NavLink to='/homepage' onClick={scrollToTop} className='nav-link fw-bold border border-dark border-1 rounded p-2 text-center'>
                                 <FaPaw mx-4/> Home
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink to='/about' className='nav-link fw-bold border border-dark border-1 rounded p-2 text-center'>
+                            <NavLink to='/about' onClick={scrollToTop} className='nav-link fw-bold border border-dark border-1 rounded p-2 text-center'>
                                 About <FaPaw mx-4/>
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink to='/product' className='nav-link fw-bold border border-dark border-1 rounded p-2 text-center'>
+                            <NavLink to='/product' onClick={scrollToTop} className='nav-link fw-bold border border-dark border-1 rounded p-2 text-center'>
                                 Collections <FaPaw mx-4/>
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink to='/contact' className='nav-link fw-bold border border-dark border-1 rounded p-2 text-center'>
+                            <NavLink to='/contact' onClick={scrollToTop} className='nav-link fw-bold border border-dark border-1 rounded p-2 text-center'>
                             <FaPaw mx-4/> Contact
                             </NavLink>
                         </NavItem>
