6d506c54b6e85ef6c9fc822f28fcdf25f909881c
Osman Ordu <orduosmann@gmail.com>
2022-10-23 12:54:38 +0300

    scroll function edited

--- src/components/header/navbar.jsx
+++ src/components/header/navbar.jsx
@@ -10,7 +10,9 @@ import { FaPaw } from 'react-icons/fa';
 const Header = () => {
     const [collapsed, setCollapsed] = useState(true);
     const scrollToTop = () => {
-        document.getElementById('scroller').scroll(0,0)
+        document.getElementById('body').scrollTo({ top: 0, behavior: 
+            'smooth' 
+          })
     }
 
     const toggleNavbar = () => setCollapsed(!collapsed);
@@ -24,7 +26,7 @@ const Header = () => {
                 <Collapse isOpen={!collapsed} navbar>
                     <Nav navbar>
                         <NavItem>
-                            <NavLink to='/homepage' onClick={scrollToTop} className='nav-link fw-bold border border-dark border-1 rounded p-2 text-center'>
+                            <NavLink to='/homepage' onClick={ scrollToTop} className='nav-link fw-bold border border-dark border-1 rounded p-2 text-center'>
                                 <FaPaw mx-4/> Home
                             </NavLink>
                         </NavItem>
@@ -34,12 +36,12 @@ const Header = () => {
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink to='/product' onClick={scrollToTop} className='nav-link fw-bold border border-dark border-1 rounded p-2 text-center'>
+                            <NavLink to='/product' onClick={ scrollToTop} className='nav-link fw-bold border border-dark border-1 rounded p-2 text-center'>
                                 Collections <FaPaw mx-4/>
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink to='/contact' onClick={scrollToTop} className='nav-link fw-bold border border-dark border-1 rounded p-2 text-center'>
+                            <NavLink to='/contact' onClick={ scrollToTop} className='nav-link fw-bold border border-dark border-1 rounded p-2 text-center'>
                             <FaPaw mx-4/> Contact
                             </NavLink>
                         </NavItem>
